db5ff448d6e05f53df4aa3a51b91dcf15645d772
Terrance Niechciol <TNiechciol@athinkingape.com>
2014-04-08 16:28:26 -0700

    Initial commit.

---
+++ android/src/com/ashenrider/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ashenrider.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ashenrider.game.HackathonGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HackathonGame(), config);
+	}
+}
