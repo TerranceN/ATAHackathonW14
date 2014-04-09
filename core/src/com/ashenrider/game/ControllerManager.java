ce791598166e2f7cd0aababc1ffb8f0fa89eaa45
Geoffrey Gollmer <ggollmer@athinkingape.com>
2014-04-08 22:40:35 -0700

    Working on controller mappings

--- core/src/com/ashenrider/game/ControllerManager.java
+++
@@ -1,4 +0,0 @@
-package com.ashenrider.game;
-
-public class ControllerManager {
-}
---
+++ core/src/com/ashenrider/game/controls/Xbox.java
@@ -0,0 +1,34 @@
+package com.ashenrider.game.controls;
+
+public class Xbox {
+    //BUTTON
+    public static final int BTN_DPAD_UP = 0;
+    public static final int BTN_DPAD_DOWN = 1;
+    public static final int BTN_DPAD_LEFT = 2;
+    public static final int BTN_DPAD_RIGHT = 3;
+
+    public static final int BTN_START = 4;
+    public static final int BTN_BACK = 5;
+
+    public static final int BTN_LEFT_STICK = 6;
+    public static final int BTN_RIGHT_STICK = 7;
+
+    public static final int BTN_LEFT_BUMPER = 8;
+    public static final int BTN_RIGHT_BUMPER = 9;
+
+    public static final int BTN_CENTER = 10;
+
+    public static final int BTN_A = 11;
+    public static final int BTN_B = 12;
+    public static final int BTN_X = 13;
+    public static final int BTN_Y = 14;
+
+    //AXIS
+    public static final int AXIS_LEFT_TRIGGER = 0;
+    public static final int AXIS_RIGHT_TRIGGER = 1;
+
+    public static final int AXIS_LEFT_STICK_HORIZONTAL = 2;
+    public static final int AXIS_LEFT_STICK_VERTICAL = 3;
+    public static final int AXIS_RIGHT_STICK_HORIZONTAL = 4;
+    public static final int AXIS_RIGHT_STICK_VERTICAL = 5;
+}
