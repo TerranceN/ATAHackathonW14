c19dfeb752153e371589fae132785677a8ed577d
Geoffrey Gollmer <ggollmer@athinkingape.com>
2014-04-08 21:26:30 -0700

    Added controller libs

---
+++ core/src/com/ashenrider/game/ControllerManager.java
@@ -0,0 +1,4 @@
+package com.ashenrider.game;
+
+public class ControllerManager {
+}
--- core/src/com/ashenrider/game/HackathonGame.java
+++ core/src/com/ashenrider/game/HackathonGame.java
@@ -2,6 +2,7 @@ package com.ashenrider.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -26,7 +27,7 @@ public class HackathonGame extends ApplicationAdapter {
         camera = new OrthographicCamera();
 		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.update();
-	}
+    }
 
 	@Override
 	public void render () {
