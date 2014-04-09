992de24a79b900ce14ee2f27fe3d54e33c0b5586
Terrance Niechciol <TNiechciol@athinkingape.com>
2014-04-08 18:22:39 -0700

    Added basic tile map support

--- core/src/com/ashenrider/game/HackathonGame.java
+++ core/src/com/ashenrider/game/HackathonGame.java
@@ -6,22 +6,36 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import com.badlogic.gdx.maps.tiled.*;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
 public class HackathonGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+    TiledMap map;
+    OrthogonalTiledMapRenderer tiledRenderer;
+    OrthographicCamera camera;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        String file = "test.tmx";
+        map = new TmxMapLoader().load(file);
+        TiledMapTileLayer layer = (TiledMapTileLayer)map.getLayers().get(0);
+        tiledRenderer = new OrthogonalTiledMapRenderer(map, Gdx.graphics.getHeight() / (100f * 32f));
+        System.out.println(layer.getWidth() + ", " + layer.getHeight());
+
+        camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.update();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+        camera.update();
+
+        tiledRenderer.setView(camera);
+        tiledRenderer.render();
 	}
 }
