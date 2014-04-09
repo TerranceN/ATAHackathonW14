cf30d93de9f76508daf352469ee45c99e1012964
Geoffrey Gollmer <ggollmer@athinkingape.com>
2014-04-09 14:11:13 -0700

    More layout

--- core/src/com/ashenrider/game/userinterface/MainMenuScreen.java
+++ core/src/com/ashenrider/game/userinterface/MainMenuScreen.java
@@ -4,6 +4,7 @@ import com.ashenrider.game.GameScreen;
 import com.ashenrider.game.HackathonApp;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -31,6 +32,7 @@ public class MainMenuScreen implements Screen {
         this.app = app;
 
         BitmapFont buttonFont = new BitmapFont();
+        buttonFont.setScale(2.0f);
 
         atlas = new TextureAtlas(Gdx.files.internal("pack/gui.atlas"));
 
@@ -42,6 +44,7 @@ public class MainMenuScreen implements Screen {
         style.disabled = new NinePatchDrawable(atlas.createPatch("ashenrider_btn_disabled"));
         style.over = new NinePatchDrawable(atlas.createPatch("ashenrider_btn_highlight"));
         style.font = buttonFont;
+        style.fontColor = new Color(1, 1, 1, 1);
 
         skin.add("default", style);
 
@@ -54,10 +57,8 @@ public class MainMenuScreen implements Screen {
         Table menu = new Table();
         menu.setFillParent(true);
         stage.addActor(menu);
-        menu.setBackground(new NinePatchDrawable(atlas.createPatch("ashenrider_btn_failed")));
 
         TextButton startButton = new TextButton("Start", skin);
-        startButton.setWidth(400);
         startButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
@@ -65,12 +66,24 @@ public class MainMenuScreen implements Screen {
             }
         });
 
-        menu.add(startButton);
+        TextButton exitButton = new TextButton("Exit", skin);
+        exitButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+
+        menu.add(startButton).width(300).height(50).padBottom(10);
+        menu.row();
+        menu.add(exitButton).width(300).height(50).padBottom(10);
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
 
@@ -84,26 +97,27 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void show() {
-
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void hide() {
-
+        Gdx.input.setInputProcessor(null);
     }
 
     @Override
     public void pause() {
-
+        hide();
     }
 
     @Override
     public void resume() {
-
+        show();
     }
 
     @Override
     public void dispose() {
+        Gdx.input.setInputProcessor(null);
         stage.dispose();
     }
 }
