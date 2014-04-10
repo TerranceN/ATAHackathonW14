08e16adde2e08a58465457b2ab14bfa515e65c38
Matthew Hyndman <myndman@gmail.com>
2014-04-10 01:04:51 -0700

    don't build ios, and controller/ keyboard menu input

--- core/src/com/ashenrider/game/HackathonApp.java
+++ core/src/com/ashenrider/game/HackathonApp.java
@@ -1,8 +1,21 @@
 package com.ashenrider.game;
 
+import com.ashenrider.game.Input.ControllerAxis;
+import com.ashenrider.game.Input.ControllerAxisButton;
+import com.ashenrider.game.Input.ControllerButton;
+import com.ashenrider.game.Input.InputAxis;
+import com.ashenrider.game.Input.InputButton;
+import com.ashenrider.game.Input.KeyboardAxis;
+import com.ashenrider.game.Input.KeyboardButton;
+import com.ashenrider.game.Input.Xbox;
 import com.ashenrider.game.userinterface.MainMenuScreen;
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 
@@ -10,7 +23,7 @@ public class HackathonApp extends Game {
 
     Stack<Screen> backStack;
     private boolean isEscaping = false;
-
+    
     @Override
 	public void create () {
         backStack = new Stack<Screen>();
--- core/src/com/ashenrider/game/userinterface/MainMenuScreen.java
+++ core/src/com/ashenrider/game/userinterface/MainMenuScreen.java
@@ -1,10 +1,21 @@
 package com.ashenrider.game.userinterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.ashenrider.game.GameScreen;
 import com.ashenrider.game.HackathonApp;
+import com.ashenrider.game.Input.ControllerAxis;
+import com.ashenrider.game.Input.ControllerButton;
+import com.ashenrider.game.Input.InputAxis;
+import com.ashenrider.game.Input.InputButton;
+import com.ashenrider.game.Input.KeyboardAxis;
+import com.ashenrider.game.Input.KeyboardButton;
 import com.ashenrider.game.Input.Xbox;
+import com.ashenrider.game.Input.XboxWindows;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.ControllerListener;
 import com.badlogic.gdx.controllers.Controllers;
@@ -19,17 +30,29 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class MainMenuScreen implements Screen {
 
-    private ControllerListener listener;
     private HackathonApp app;
     private TextureAtlas atlas;
     private Stage stage;
     private Skin skin;
+    private ArrayList<Button> buttons;
+    
+    private List<InputAxis> hAxis;
+    private List<InputAxis> vAxis;
+    private List<InputButton> select;
+    private List<InputButton> back;
 
+    private float btnIndex; // floor to get actual button index
+    private float navSpeed = 4.0f; // buttons per second
+
+    TextButton.TextButtonStyle style;
+    TextButton.TextButtonStyle selectedStyle;
+    
     public MainMenuScreen(final HackathonApp app) {
         this.app = app;
 
@@ -39,13 +62,21 @@ public class MainMenuScreen implements Screen {
 
         skin = new Skin();
 
-        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        style = new TextButton.TextButtonStyle();
         style.up = new NinePatchDrawable(atlas.createPatch("ashenrider_btn"));
         style.down = new NinePatchDrawable(atlas.createPatch("ashenrider_btn_pressed"));
         style.disabled = new NinePatchDrawable(atlas.createPatch("ashenrider_btn_disabled"));
         style.over = new NinePatchDrawable(atlas.createPatch("ashenrider_btn_highlight"));
         style.font = buttonFont;
         style.fontColor = new Color(1, 1, 1, 1);
+
+        selectedStyle = new TextButton.TextButtonStyle();
+        selectedStyle.up = new NinePatchDrawable(atlas.createPatch("ashenrider_btn_highlight"));
+        selectedStyle.down = new NinePatchDrawable(atlas.createPatch("ashenrider_btn_pressed"));
+        selectedStyle.disabled = new NinePatchDrawable(atlas.createPatch("ashenrider_btn_highlight"));
+        selectedStyle.over = new NinePatchDrawable(atlas.createPatch("ashenrider_btn_highlight"));
+        selectedStyle.font = buttonFont;
+        selectedStyle.fontColor = new Color(1, 1, 1, 1);
 
         skin.add("default", style);
 
@@ -75,61 +106,33 @@ public class MainMenuScreen implements Screen {
             }
         });
 
+        btnIndex = 0.0f;
+        buttons = new ArrayList<Button>();
+        buttons.add(startButton);
         menu.add(startButton).width(300).height(50).padBottom(10);
         menu.row();
         menu.add(exitButton).width(300).height(50).padBottom(10);
+        buttons.add(exitButton);
 
-        listener = new ControllerListener() {
-            @Override
-            public void connected(Controller controller) {
+        hAxis = new ArrayList<InputAxis>();
+        vAxis = new ArrayList<InputAxis>();
+        select = new ArrayList<InputButton>();
+        back = new ArrayList<InputButton>();
+        
+        hAxis.add(new KeyboardAxis(Keys.A, Keys.D));
+        hAxis.add(new KeyboardAxis(Keys.LEFT, Keys.RIGHT));
+        vAxis.add(new KeyboardAxis(Keys.S, Keys.W));
+        vAxis.add(new KeyboardAxis(Keys.DOWN, Keys.UP));
 
-            }
-
-            @Override
-            public void disconnected(Controller controller) {
-
-            }
-
-            @Override
-            public boolean buttonDown(Controller controller, int i) {
-                if(i == Xbox.BTN_A || i == Xbox.BTN_START) {
-                    startGame();
-                } else if (i == Xbox.BTN_B || i == Xbox.BTN_BACK) {
-                    exitGame();
-                }
-                return false;
-            }
-
-            @Override
-            public boolean buttonUp(Controller controller, int i) {
-                return false;
-            }
-
-            @Override
-            public boolean axisMoved(Controller controller, int i, float v) {
-                return false;
-            }
-
-            @Override
-            public boolean povMoved(Controller controller, int i, PovDirection povDirection) {
-                return false;
-            }
-
-            @Override
-            public boolean xSliderMoved(Controller controller, int i, boolean b) {
-                return false;
-            }
-
-            @Override
-            public boolean ySliderMoved(Controller controller, int i, boolean b) {
-                return false;
-            }
-
-            @Override
-            public boolean accelerometerMoved(Controller controller, int i, Vector3 vector3) {
-                return false;
-            }
-        };
+        select.add(new KeyboardButton(Keys.ENTER));
+        back.add(new KeyboardButton(Keys.ESCAPE));
+        
+        for(Controller controller : Controllers.getControllers()) {
+            hAxis.add(new ControllerAxis(controller, XboxWindows.AXIS_LEFT_STICK_HORIZONTAL));
+            vAxis.add(new ControllerAxis(controller, XboxWindows.AXIS_LEFT_STICK_VERTICAL, true));
+            select.add(new ControllerButton(controller, XboxWindows.BTN_A));
+            back.add(new ControllerButton(controller, XboxWindows.BTN_B));
+        }
     }
 
     public void startGame() {
@@ -140,12 +143,64 @@ public class MainMenuScreen implements Screen {
         Gdx.app.exit();
     }
 
+    private void update(float delta) {
+        //
+        for (InputAxis iAxis : hAxis) {
+        	if (iAxis.getValue() > 0.5f) {
+                Gdx.app.log("input", "Go right");
+        	} else if (iAxis.getValue() < -0.5f) {
+                Gdx.app.log("input", "Go left");
+        	}
+        }
+        for (InputAxis iAxis : vAxis) {
+        	if (Math.abs(iAxis.getValue()) > 0.2f) {
+        		btnIndex += iAxis.getValue() * navSpeed * delta;
+        		if (btnIndex < 0) {
+        			btnIndex += buttons.size();
+        		} else {
+            		btnIndex = btnIndex % buttons.size();
+        		}
+        		Gdx.app.log("Button index", "btn index: " + btnIndex);
+        	}
+        	if (iAxis.getValue() > 0.5f) {
+                Gdx.app.log("input", "Go up");
+        	} else if (iAxis.getValue() < -0.5f) {
+                Gdx.app.log("input", "Go down");
+        	}
+        }
+        for (InputButton iButton : select) {
+        	if (iButton.isDown()) {
+                Gdx.app.log("input", "Ok");
+            	if (Math.floor(btnIndex) == 0) {
+            		startGame();
+            	} else {
+                    exitGame();
+            	}
+        	}
+        }
+        for (InputButton iButton : back) {
+        	if (iButton.isDown()) {
+                Gdx.app.log("input", "Back");
+                //exitGame();
+        	}
+        }
+        for (int i=0; i<buttons.size(); i++) {
+        	Button btn = buttons.get(i);
+        	if (i == Math.floor(btnIndex)) {
+        		btn.setStyle(selectedStyle);
+        	} else {
+        		btn.setStyle(style);
+        	}
+        }
+    }
+    
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act(delta);
+        update(delta);
         stage.draw();
 
         Table.drawDebug(stage);
@@ -159,13 +214,11 @@ public class MainMenuScreen implements Screen {
     @Override
     public void show() {
         Gdx.input.setInputProcessor(stage);
-        Controllers.addListener(listener);
     }
 
     @Override
     public void hide() {
         Gdx.input.setInputProcessor(null);
-        Controllers.removeListener(listener);
     }
 
     @Override
